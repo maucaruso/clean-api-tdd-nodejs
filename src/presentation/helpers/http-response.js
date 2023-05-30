39427b5f029719d9fa65068b9133cb11ffe9f2e5
maucaruso <carusojr@outlook.com>
2023-05-30 07:06:35 -0300

    modify HttpResponse.badRequest to receive an error

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,12 +1,11 @@
-const MissigParamError = require('./missing-param-error')
 const UnauthorizedError = require('./unauthorized-error')
 const ServerError = require('./server-error')
 
 module.exports = class HttpResponse {
-  static badRequest (paramName) {
+  static badRequest (error) {
     return {
       statusCode: 400,
-      body: new MissigParamError(paramName)
+      body: error
     }
   }
 
--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -169,4 +169,19 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
+
+  // it('Should return 400 if an invalid email is provided', async () => {
+  //   const { sut } = makeSut()
+
+  //   const httpRequest = {
+  //     body: {
+  //       email: 'invalid_email@mail.com',
+  //       password: 'any_password'
+  //     }
+  //   }
+
+  //   const httpResponse = await sut.route(httpRequest)
+  //   expect(httpResponse.statusCode).toBe(400)
+  //   expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  // })
 })
